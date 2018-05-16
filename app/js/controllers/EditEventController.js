bc1d38ad23a85dda5f7671d1fcf96811d71cf7ea
JMART <register@truncator.com>
2018-05-15 19:36:42 -0500

    Fixed data save

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -4,14 +4,17 @@
 eventsApp.controller('EditEventController',
     function EditEventController($scope, eventData){
 
+      $scope.event = {
+        name:"New Event",
+        date:"01/01/2019"
+      };
         
         $scope.saveEvent = function (event, newEventForm) {
             if(newEventForm.$valid){
                  eventData.save(event)
-                    .$promise.then(
-                        function(response) { console.log('success', response)},
-                        function(response) { console.log('failure', response)}
-                    );
+                    .$promise.then(function(response) { 
+                        console.log('success', response)
+                    });
 
              }
         }
